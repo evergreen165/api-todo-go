f6298eed33ae7e4c0182512b6e10e36625b85a70
antonioGabrielGomes <antonioggca000@gmail.com>
2025-09-26 01:40:35 -0300

    feat: get todo

---
+++ models/get.go
@@ -0,0 +1,17 @@
+package models
+
+import "github.com/antonioGabrielGomes/api-todo-go/db"
+
+func Get(id int64) (todo Todo, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	row := conn.QueryRow(`SELECT * FROM todos WHERE id=$id`, id)
+
+	err = row.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+
+	return
+}
