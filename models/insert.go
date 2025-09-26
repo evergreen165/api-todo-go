b74322839cbf6b9e9b27e56a4be9c55a11b3977f
antonioGabrielGomes <antonioggca000@gmail.com>
2025-09-26 01:37:34 -0300

    feat: insert

--- models/insert.go
+++ models/insert.go
@@ -9,7 +9,7 @@ func Insert(todo Todo) (id int64, err error) {
 	}
 	defer conn.Close()
 
-	sql := `INSERT INTO todos (title, description, done) VALUES ($1, $2, $3) RETURN`
+	sql := `INSERT INTO todos (title, description, done) VALUES ($1, $2, $3) RETURNING id`
 
 	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
 
